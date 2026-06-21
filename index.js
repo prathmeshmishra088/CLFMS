1e94884f018c0eb28cfcf604b3ad20dcc0a46635
prathmeshmishra088 <prathmeshmishra088@gmail.com>
2026-06-21 16:27:50 +0530

    Remove node_modules and .env from repository

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const lostItemRoutes = require("./routes/lostItemRoutes");
 const foundItemRoutes = require("./routes/foundItemRoutes");
 const claimRoutes = require("./routes/claimRoutes");
 
@@ -8,8 +11,6 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const lostItemRoutes = require("./routes/lostItemRoutes");
-const authRoutes = require("./routes/authRoutes");
 
 connectDB();
 
@@ -17,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/lost", lostItemRoutes);
 app.use("/api/found", foundItemRoutes);
 app.use("/api/claim", claimRoutes);
 
@@ -25,5 +28,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-app.use("/api/auth", authRoutes);
-app.use("/api/lost", lostItemRoutes);
