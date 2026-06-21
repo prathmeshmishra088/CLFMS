8c75c61d122d1b1eb778859823c82a2aa7ef8d84
pranitapawar2009-boop <pranitapawar2009@gmail.com>
2026-06-21 15:32:43 +0530

    Complete User and LostItem module

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+const lostItemRoutes = require("./routes/lostItemRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 connectDB();
 
@@ -18,3 +20,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+app.use("/api/auth", authRoutes);
+app.use("/api/lost", lostItemRoutes);
---
+++ models/LostItem.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const lostItemSchema = new mongoose.Schema(
+{
+  itemName:{
+    type:String,
+    required:true
+  },
+  description:{
+    type:String,
+    required:true
+  },
+  location:{
+    type:String,
+    required:true
+  }
+},
+{ timestamps:true }
+);
+
+module.exports = mongoose.model(
+  "LostItem",
+  lostItemSchema
+);
